2be7b7654fc3f29c1eb1848af393cc77b0e927b8
flk <0615@flk.com>
2023-02-17 20:28:21 +0800

    version 3.0

--- src/main/java/com/flk/git/GitTest.java
+++ src/main/java/com/flk/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git！");
         System.out.println("version 2.0");
+        System.out.println("version 3.0");
     }
 }
