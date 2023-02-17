435a2fe8d1e2632bdb607a9a2f724c5fcb93d342
flk <0615@flk.com>
2023-02-17 20:26:37 +0800

    version 1.0

---
+++ src/main/java/com/flk/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.flk.git;
+
+/**
+ * @author FLK
+ * @create 2023-02-17 20:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git！");
+    }
+}
