2f34c49e9abe3acd1bb71544b13f73425bf52564
flk <0615@flk.com>
2023-02-17 20:33:44 +0800

    version 3.0-hof-fix

--- src/main/java/com/flk/git/GitTest.java
+++ src/main/java/com/flk/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git！");
         System.out.println("version 2.0");
         System.out.println("version 3.0");
+        System.out.println("hof-fix");
     }
 }
